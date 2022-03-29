c7a611e0247e0acab0958a700843482971965b9a
KillShotGhost <coolghostuni@gmail.com>
2022-03-29 17:31:06 +0530

    find minimum and maximum in an array

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 Arrays
 
-1. [Reverse the Array](https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/) 
+1. [Reverse the Array](https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/)
+2. [Find the maximum and minimum element in an array](https://www.geeksforgeeks.org/maximum-and-minimum-in-an-array/)
--- src/main/java/Arrays/day_2/FindMaximumAndMinimumInArray.java
+++ src/main/java/Arrays/day_2/FindMaximumAndMinimumInArray.java
@@ -1,4 +1,28 @@
 package Arrays.day_2;
 
 public class FindMaximumAndMinimumInArray {
+    public static void main(String[] args) {
+        int[] array1 = {2, 8, 4, 1, 6, 5, 3, 4};
+        int[] array2 = {2, 8, 4, 1, 6, 5, 3, 4, 9};
+        findMinimumAndMaximum(array1);
+        findMinimumAndMaximum(array2);
+    }
+
+    private static void findMinimumAndMaximum(int[] array) {
+        if (array.length == 0) {
+            System.out.println("Array is Empty");
+        }
+        int min = array[0];
+        int max = array[array.length - 1];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        System.out.println("min = " + min);
+        System.out.println("max = " + max);
+    }
 }

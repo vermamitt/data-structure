cfdebce30536077b3d893253f4faf68ec9a0908f
KillShotGhost <coolghostuni@gmail.com>
2022-03-30 09:47:30 +0530

    kth min max element in an array

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Arrays
 
 1. [Reverse the Array](https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/)
 2. [Find the maximum and minimum element in an array](https://www.geeksforgeeks.org/maximum-and-minimum-in-an-array/)
+3. [Find the "Kth" max and min element of an array ](https://practice.geeksforgeeks.org/problems/kth-smallest-element5635/1)
---
+++ src/main/java/Arrays/day_3/KthMaxMinElement.java
@@ -0,0 +1,26 @@
+package Arrays.day_3;
+
+import java.util.Arrays;
+
+public class KthMaxMinElement {
+
+    public static void main(String[] args) {
+        int array[] = {2, 8, 4, 1, 6, 5, 3, 4, 9};
+        findKthMaxMin(array, 2);
+        findKthMaxMin(array, 3);
+    }
+
+    private static void findKthMaxMin(int[] array, int k) {
+        Arrays.sort(array);
+        /*
+        Arrays.sort uses dual-pivot quick sort algorithm internally
+        Typically it's faster than traditional(one pivot) quick sort implementation
+        */
+        for (int element : array) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+        System.out.println("Kth min : " + array[k - 1]);
+        System.out.println("Kth max : " + array[array.length - k]);
+    }
+}

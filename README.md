317ec978f8e40c90809f15a0a81bac77188e056c
KillShotGhost <coolghostuni@gmail.com>
2022-03-30 20:06:56 +0530

    sort an array of 0 1 2 without sorting alogorithm

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Arrays
 1. [Reverse the Array](https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/)
 2. [Find the maximum and minimum element in an array](https://www.geeksforgeeks.org/maximum-and-minimum-in-an-array/)
 3. [Find the "Kth" max and min element of an array ](https://practice.geeksforgeeks.org/problems/kth-smallest-element5635/1)
+4. [Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algo](https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1)
---
+++ src/main/java/Arrays/day_4/SortArrayOf0s1s2s.java
@@ -0,0 +1,36 @@
+package Arrays.day_4;
+
+public class SortArrayOf0s1s2s {
+    public static void main(String[] args) {
+        int array[] = {0, 2, 1, 2, 0};
+        int array1[] = {0, 2, 1, 2, 0, 0, 0, 2, 2, 2, 1, 1, 1, 0, 1, 1, 0, 2};
+        sortArray(array);
+        sortArray(array1);
+    }
+
+    private static void sortArray(int[] array) {
+        int zeroesCount = 0;
+        int onesCount = 0;
+        int twosCount = 0;
+        for (int element : array) {
+            if (element == 0) zeroesCount++;
+            if (element == 1) onesCount++;
+            if (element == 2) twosCount++;
+        }
+        swap(array, zeroesCount, 0, 0);
+        swap(array, onesCount, zeroesCount, 1);
+        swap(array, twosCount, zeroesCount + onesCount, 2);
+        for (int element : array) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+
+    private static void swap(int[] array, int count, int startPosition, int value) {
+        while (count != 0) {
+            array[startPosition] = value;
+            startPosition++;
+            count--;
+        }
+    }
+}
